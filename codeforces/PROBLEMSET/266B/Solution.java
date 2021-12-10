a05c7da80a3572665fffd8268d94fc516abceac7
danny329 <danielpaul329@outlook.com>
2021-12-10 11:56:02 +0530

    266B

---
+++ codeforces/PROBLEMSET/266B/Solution.java
@@ -0,0 +1,27 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputs = br.readLine().split(" ");
+        int n = Integer.parseInt(inputs[0]);
+        int t = Integer.parseInt(inputs[1]);
+        char[] queue = br.readLine().toCharArray();
+        if(queue.length == n){
+            for(int i=0; i<t; i++){
+                for(int j=0; j< queue.length; j++){
+                    if(queue[j] == 'B' && queue[j+1] =='G'){
+                        queue[j] = 'G';
+                        queue[j+1] = 'B';
+                        j++;
+                    }
+                }
+            }
+        }
+        System.out.println(String.valueOf(queue));
+    }
+}
